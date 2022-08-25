880f1ade5e723caec4e315c43a362dae62b1d908
Vaibhav Matere <vpm00718897@gmail.com>
2022-08-25 14:20:52 +0530

    refactor and install dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>INVOICE GENERATOR</h1>
+      <h1>Invoice Generator</h1>
     </div>
   );
 }
