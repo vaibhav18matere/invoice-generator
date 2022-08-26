81c62aa9d3c3f9108b917f64e85fb26e4d9c8a31
Vaibhav Matere <vpm00718897@gmail.com>
2022-08-26 16:47:24 +0530

    feat : state management for all components

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ function App() {
   const [address, setAddress] = useState("");
   const [email, setEmail] = useState("");
   const [showInvoice, setShowInvoice] = useState(false);
+  const [clientName, setClientName] = useState("");
+  const [clientAddress, setClientAddress] = useState("");
+  const [invoiceNumber, setInvoiceNumber] = useState("");
+  const [invoiceNotes, setInvoiceNotes] = useState("");
+  const [invoiceDate, setInvoiceDate] = useState("");
+  const [dueDate, setDueDate] = useState("");
 
   const printHandler = () => {
     window.print();
@@ -24,10 +30,17 @@ function App() {
           <div>
             <Header printHandler={printHandler} />
             <CompanyDetails name={name} address={address} email={email} />
-            <ClientDetails />
-            <InvoiceDetails />
+            <ClientDetails
+              clientName={clientName}
+              clientAddress={clientAddress}
+            />
+            <InvoiceDetails
+              invoiceNumber={invoiceNumber}
+              invoiceDate={invoiceDate}
+              dueDate={dueDate}
+            />
             <Table />
-            <ClientNotes />
+            <ClientNotes invoiceNotes={invoiceNotes} />
             <Footer />
             <button
               className="check-invoice-btn"
@@ -71,7 +84,66 @@ function App() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
-
+              <label htmlFor="clientname">Enter Client's Name </label>
+              <input
+                type="text"
+                name="text"
+                id="clientname"
+                placeholder="type client's name"
+                autoComplete="off"
+                value={clientName}
+                onChange={(e) => setClientName(e.target.value)}
+              />
+              <label htmlFor="clientaddress">Enter Client's Address </label>
+              <input
+                type="text"
+                name="text"
+                id="clientaddress"
+                placeholder="type client's address"
+                autoComplete="off"
+                value={clientAddress}
+                onChange={(e) => setClientAddress(e.target.value)}
+              />
+              <label htmlFor="invoiceNumber">Enter invoice Number </label>
+              <input
+                type="number"
+                name="invoiceNumber"
+                id="invoiceNumber"
+                placeholder="type invoice number"
+                autoComplete="off"
+                value={invoiceNumber}
+                onChange={(e) => setInvoiceNumber(e.target.value)}
+              />
+              <label htmlFor="invoiceNotes">Enter invoice Notes </label>
+              <input
+                type="text"
+                name="invoiceNotes"
+                id="invoiceNotes"
+                placeholder="type invoice notes"
+                autoComplete="off"
+                value={invoiceNotes}
+                onChange={(e) => setInvoiceNotes(e.target.value)}
+              />
+              <label htmlFor="invoiceDate">Enter Invoice Date </label>
+              <input
+                type="date"
+                name="invoiceDate"
+                id="invoiceNumber"
+                placeholder="type invoice date"
+                autoComplete="off"
+                value={invoiceDate}
+                onChange={(e) => setInvoiceDate(e.target.value)}
+              />
+              <label htmlFor="dueDate">Enter due Date </label>
+              <input
+                type="date"
+                name="dueDate"
+                id="dueDate"
+                placeholder="type due date of invoice"
+                autoComplete="off"
+                value={dueDate}
+                onChange={(e) => setDueDate(e.target.value)}
+              />
               <button
                 onClick={() => setShowInvoice(true)}
                 className="check-invoice-btn"
