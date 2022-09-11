c68290a8a72112999fcd67f867b863a6232447c2
Vaibhav Matere <vpm00718897@gmail.com>
2022-09-12 01:52:33 +0530

    feat : toast added as a sent mail

--- src/components/ClientDetails/ClientDetails.jsx
+++ src/components/ClientDetails/ClientDetails.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 import "./ClientDetails.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ClientDetails = ({ clientName, clientAddress, clientEmail }) => {
-  const handleClientDetail = (e) => {
-    e.preventDefault();
+  const sendMailHandlerToast = () => {
+    toast("Email sent to user");
   };
 
   return (
-    <form className="client-section" onSubmit={handleClientDetail}>
+    <div className="client-section">
       <p>
         Client Name : <span className="client-data-input">{clientName}</span>
       </p>
       <p>
-        Client Address : <span className="client-data-input">{clientAddress}</span>
+        Client Address :{" "}
+        <span className="client-data-input">{clientAddress}</span>
       </p>
       <p>
-        Client's Email : <span className="client-data-input">{clientEmail}</span>
+        Client's Email :{" "}
+        <span className="client-data-input">{clientEmail}</span>
       </p>
-      <button className="send-email">Send E-Mail to Client</button>
-    </form>
+      <button className="send-email" onClick={sendMailHandlerToast}>
+        Send E-Mail to Client
+      </button>
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
   );
 };
 
